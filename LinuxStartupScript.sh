74507b9bc08025a0ab28e48cc714e085da3f715c
bactran1 <56278118+bactran1@users.noreply.github.com>
2024-10-13 00:43:54 -0700

    test1

--- LinuxStartupScript.sh
+++ LinuxStartupScript.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install openssh-server -y
+#echo "Starting to update and upgrade..........................."
+#sudo apt-get update && sudo apt-get upgrade -y &&\
+#echo "Starting to install ssh server..........................."
+#sudo apt-get install openssh-server -y\
+#echo "Starting to Docker Engine..........................."
+if lsb_release -r | grep -q "Ubuntu"
+then
+    echo "Found Ubuntu."
+else
+  echo "Found somthing else."
+fi
